d9047ab20220ce2533e4c3b703c86d6fc57521b4
Coleman Sandefur <coleman.sandefur@gmail.com>
2026-01-30 08:28:22 -0600

    Backup

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,50 @@
+return {
+    -- nvim-cmp for auto-completion
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",  -- Lazy load on entering Insert mode
+        dependencies = {
+            -- Completion sources
+            "hrsh7th/cmp-nvim-lsp",  -- LSP source for nvim-cmp
+            "hrsh7th/cmp-buffer",     -- Buffer source for nvim-cmp
+            "hrsh7th/cmp-path",       -- Path source for nvim-cmp
+            "hrsh7th/cmp-cmdline",    -- Cmdline source for nvim-cmp
+            "saadparwaiz1/cmp_luasnip", -- LuaSnip source (for snippets)
+
+        },
+        config = function()
+            -- nvim-cmp setup
+            local cmp = require("cmp")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        -- Use LuaSnip for snippets (if you want to use snippets)
+                        vim.fn["vsnip#anonymous"](args.body)
+                    end,
+                },
+                mapping = {
+                    -- Open the completion menu using Ctrl + Space
+                    ["<C-Space>"] = cmp.mapping.complete(),
+
+                    -- Confirm auto complete using Tab
+                    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+
+                    -- Navigate the suggestions
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-u>"] = cmp.mapping.scroll_docs(4),
+                },
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                    { name = "cmdline" },
+                    { name = "luasnip" },  -- Optional, if using LuaSnip
+                },
+            })
+        end,
+    },
+}
+
